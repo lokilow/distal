c7f6822debe38e0679640d78b48ac24635d8d859
lokilow <lokilow@pm.me>
2024-08-22 14:37:20 -0400

    parse cli arg and add rustfmt.toml

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,15 @@
 use actix::prelude::*;
+use std::env;
 mod collatz;
-
 
 #[actix::main]
 async fn main() {
+    let initial_value = env::args().nth(1).expect("10").parse::<usize>().unwrap();
     // start new actor
-    let addr = collatz::CollatzActor { count: 10 }.start();
+    let addr = collatz::CollatzActor {
+        count: initial_value,
+    }
+    .start();
 
     // send message and get future for result
     let res = addr.send(collatz::Ping(10)).await;
